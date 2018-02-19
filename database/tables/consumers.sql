13e7f6f7769a5164685844c8ea80d3bfec5163c1
Julien Escueta <julien.escueta@gmail.com>
2018-02-18 19:27:05 -0800

    database schema and migrations to be handled in sequelize

--- database/tables/consumers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE consumers (
-       id  uuid       PRIMARY KEY,
-     name  text       NOT NULL,
-     size  int        NULL,
-  created  timestamp  NOT NULL,
-  updated  timestamp  NULL,
-  deleted  timestamp  NULL
-);
